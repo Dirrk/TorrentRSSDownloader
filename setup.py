5fb71f055e301b7c44d05ba5798fe438a511d7bc
Dirrk <Derekrada@gmail.com>
2015-03-10 20:33:09 -0400

    VERSION 1.2.1  Cleaning up prints to logging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TorrentRSSDownloader',
-      version='1.2',
+      version='1.2.1',
       description='Torrent RSS Downloader',
       author='Derek Rada',
       url='https://github.com/Dirrk/TorrentRSSDownloader',
